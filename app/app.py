c6554b0461d91a58ba0339ade2d8085d9f71fa46
freija <freija@gmail.com>
2017-09-06 10:27:18 -0700

    Change default port and remove traefik label

--- app/app.py
+++ app/app.py
@@ -150,4 +150,4 @@ api.add_resource(recommendAPI, '/api/recommend/v1.0/<corpus_name>',
                  resource_class_args=[recommenders])
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5010, debug=False)
